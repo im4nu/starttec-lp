76850da73a70932ed342d039f618b6e0a021e786
im4nu <victorx811@gmail.com>
2023-12-01 18:24:09 -0300

    New products on products screen

--- src/utils/products.ts
+++ src/utils/products.ts
@@ -12,7 +12,7 @@ export const productsData = [
     title: "StartMessage - Sistema de envio de mensagens",
     description: "Sistema de disparo de mensagens automáticas.",
     image: "/images/startmessage.png",
-    launchDate: "8 de fev 2018",
+    launchDate: "12 de mar 2019",
     link: "https://www.startmessage.com.br/",
     id: 1,
   },
@@ -21,9 +21,18 @@ export const productsData = [
     description:
       "Evite multas severas pela não adequação DA SUA ORGANIZAÇÃO À LGPD!",
     image: "/images/hub-dpo.png",
-    launchDate: "14 de set 2023",
+    launchDate: "25 de jun 2020",
     link: "https://www.hubdpo.com.br/",
     id: 2,
+  },
+  {
+    title: "Finset - Sistema de Gestão Financeira",
+    description:
+      "A gestão que falta em sua empresa. Controle de contas a pagar, contas a receber, fluxo de caixa, conciliação bancária e muito mais.",
+    image: "/images/finset.svg",
+    launchDate: "15 de out 2021",
+    link: "https://www.finset.com.br/",
+    id: 3,
   },
   {
     title: "EventoSmart - Sistema de Gestão de Eventos",
@@ -32,15 +41,33 @@ export const productsData = [
     image: "/images/eventoSmart.png",
     launchDate: "14 de set 2023",
     link: "https://www.eventosmart.com.br/",
-    id: 3,
+    id: 4,
   },
   {
-    title: "Finset - Sistema de Gestão Financeira",
+    title: "Grupo W - Sistema de Networking feminino",
     description:
-      "A gestão que falta em sua empresa. Controle de contas a pagar, contas a receber, fluxo de caixa, conciliação bancária e muito mais.",
-    image: "/images/finset.svg",
-    launchDate: "14 de set 2023",
-    link: "https://www.finset.com.br/",
-    id: 4,
+      "Um sistema web para conectar mulheres empreendedoras e promover eventos do W.",
+    image: "/images/grupo-w-logo.svg",
+    launchDate: "01 de jan 2023",
+    link: "https://www.ogrupow.com.br/",
+    id: 5,
+  },
+  {
+    title: "Genius Line - Rotas inteligentes para parques de diversão",
+    description:
+      "Aplicativo para gerenciar filas de parques de diversão.",
+    image: "/images/genius-line-logo.svg",
+    launchDate: "EM BREVE",
+    link: "#",
+    id: 6,
+  },
+  {
+    title: "PersonNow - Treine onde e quando quiser!",
+    description:
+      "Aplicativo para encontrar personal trainers, marcar horários de treinos, fazer ficha de treinamento e muito mais.",
+    image: "/images/person-now-logo.svg",
+    launchDate: "EM BREVE",
+    link: "#",
+    id: 7,
   },
 ];
